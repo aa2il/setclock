57872438bb3e06209c0e67cfc301de8603543c12
aa2il <jattili@yahoo.com>
2023-04-07 16:09:51 -0700

    Updates

--- setclock.py
+++ setclock.py
@@ -2,14 +2,14 @@
 ################################################################################
 #
 # Set Clock - Rev 1.0
-# Copyright (C) 2021 by Joseph B. Attili, aa2il AT arrl DOT net
+# Copyright (C) 2021-3 by Joseph B. Attili, aa2il AT arrl DOT net
 #
 # GUI to set system date &/or time manually using a time source such as
 # a cellphone or WWV.
 #
 # Notes:
-#  - Need this library:
-#                  pip3 install tkcalendar
+#  - Need these libraries:
+#                  pip3 install tkcalendar gpsd-py3
 #
 #  - The other programs in this directory are for playing with a GPS dongle.
 #    Eventually, would like to incorporate that capability into this code.
---
+++ windoz.bat
@@ -0,0 +1,16 @@
+@echo off
+echo.
+echo Notes about how to run setclock on Windoze 10
+echo.
+echo This needs   pip install gpsd-py3  but there doesn't seem to
+ech  be a build for windoz - work on this later if we really need it.
+echo.
+echo Havent really execised this yet but seems ok so far
+echo    setclock.py
+echo.
+echo To compile (Haven't tried this yet)
+echo.
+echo         pyinstaller --onefile ftdx.py
+echo         dist\ftdx.exe
+echo.
+
